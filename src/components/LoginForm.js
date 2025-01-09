9d52c6f58f31253bd56fab385a9c9ec6542320f7
Tuhin1904 <iamtuhinn@gmail.com>
2025-01-09 22:12:02 +0530

    p tag added

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,30 +1,43 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
-const LoginForm = ({isLoggedIn,setLogin}) => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        setLogin()
-    }
+const LoginForm = ({ isLoggedIn, setLogin }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLogin();
+  };
   return (
     <div>
-    {isLoggedIn ? <h3>You are logged in!</h3> : <div>
-        <form onSubmit={handleSubmit}>
+      {isLoggedIn ? (
+        <p>You are logged in!</p>
+      ) : (
+        <div>
+          <form onSubmit={handleSubmit}>
             <label>Username:</label>
-            <input type='text' value={username} onChange={(e) => setUsername(e.target.value)} required/>
-            <br/>
-            
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+            <br />
+
             <label>Password:</label>
-            <input type='password'value={password} onChange={(e) => setPassword(e.target.value)} required/>
-           <br/>
-            <button type='submit'>Login</button>
-        </form>
-    </div>}
-    
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <br />
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default LoginForm;
